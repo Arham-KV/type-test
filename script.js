60d5b2e6a9cb2c454d35c77ef8b97baa27e857e5
Arham <syedarham032@gmai.com>
2024-01-26 22:27:25 +0500

    convert | js into | jquery

--- script.js
+++ script.js
@@ -1,169 +1,199 @@
-//Random Quotes Api URL
-const quoteApiUrl = "https://api.quotable.io/random?minLength=80&maxLength=100";
-const quoteSection = document.getElementById("quote");
-const userInput = document.getElementById("quote-input");
+// const quoteApiUrl = "https://api.quotable.io/random?minLength=80&maxLength=100";
+// const quoteSection = document.getElementById("quote");
+// const userInput = document.getElementById("quote-input");
 
-let quote = "",
-  time = 60,
-  timer = "",
-  mistakes = 0;
+// let quote = "",
+//   time = 60,
+//   timer = "",
+//   mistakes = 0;
 
-//Display random quotes
-// const renderNewQuote = async () => {
-//   //Fetch contents from url
-//   const response = await fetch(quoteApiUrl);
-
-//   //Store response
-//   let data = await response.json();
-
-//   //Access quote
-//   quote = data.content;
-
-//   //Array of characters in the quote
-//   let arr = quote.split("").map((value) => {
-//     //wrap the characters in a span tag
-//     return "<span class='quote-chars'>" + value + "</span>";
-//   });
-//   //join array for displaying
-//   quoteSection.innerHTML += arr.join("");
+// quoteSection.oncopy = function (event) {
+//   event.preventDefault();
+//   return false;
 // };
-quoteSection.oncopy = function (event) {
-  event.preventDefault();
-  return false;
-};
 
-const renderNewQuote = async () => {
-  try {
-    const { content } = await (await fetch(quoteApiUrl)).json();
-    renderQuote(content);
-  } catch (error) {
-    console.error('Error fetching quote:', error.message);
-  }
-};
+// const renderNewQuote = async () => {
+//   try {
+//     const { content } = await (await fetch(quoteApiUrl)).json();
+//     renderQuote(content);
+//   } catch (error) {
+//     console.error('Error fetching quote:', error.message);
+//   }
+// };
 
-const renderQuote = (quote) => {
-  quoteSection.innerHTML = [...quote].map(char => `<span class='quote-chars'>${char}</span>`).join('');
-};
+// const renderQuote = (quote) => {
+//   quoteSection.innerHTML = [...quote].map(char => `<span class='quote-chars'>${char}</span>`).join('');
+// };
 
-
-//Logic for comparing input words with quote
 // userInput.addEventListener("input", () => {
-//   let quoteChars = document.querySelectorAll(".quote-chars");
-//   //Create an arrat from received span tags
-//   quoteChars = Array.from(quoteChars);
-
-//   //array of user input characters
+//   let quoteChars = Array.from(document.querySelectorAll(".quote-chars"));
 //   let userInputChars = userInput.value.split("");
 
-//   //loop through each character in quote
 //   quoteChars.forEach((char, index) => {
-//     //Check if char(quote character) = userInputChars[index](input character)
-//     if (char.innerText == userInputChars[index]) {
+//     if (char.innerText === userInputChars[index]) {
 //       char.classList.add("success");
-//     }
-//     //If user hasn't entered anything or backspaced
-//     else if (userInputChars[index] == null) {
-//       //Remove class if any
-//       if (char.classList.contains("success")) {
-//         char.classList.remove("success");
-//       } else {
-//         char.classList.remove("fail");
-//       }
-//     }
-//     //If user enter wrong character
-//     else {
-//       //Checks if we alreasy have added fail class
+//     } else if (!userInputChars[index]) {
+//       char.classList.remove("success", "fail");
+//     } else {
 //       if (!char.classList.contains("fail")) {
-//         //increment and display mistakes
 //         mistakes += 1;
 //         char.classList.add("fail");
+//         document.getElementById("mistakes").innerText = mistakes;
 //       }
-//       document.getElementById("mistakes").innerText = mistakes;
-//     }
-//     //Returns true if all the characters are entered correctly
-//     let check = quoteChars.every((element) => {
-//       return element.classList.contains("success");
-//     });
-//     //End test if all characters are correct
-//     if (check) {
-//       displayResult();
 //     }
 //   });
+
+//   if (quoteChars.every((element) => element.classList.contains("success"))) {
+//     displayResult();
+//   }
 // });
 
-userInput.addEventListener("input", () => {
-  let quoteChars = Array.from(document.querySelectorAll(".quote-chars"));
-  let userInputChars = userInput.value.split("");
 
-  quoteChars.forEach((char, index) => {
-    if (char.innerText === userInputChars[index]) {
-      char.classList.add("success");
-    } else if (!userInputChars[index]) {
-      char.classList.remove("success", "fail");
-    } else {
-      if (!char.classList.contains("fail")) {
-        mistakes += 1;
-        char.classList.add("fail");
-        document.getElementById("mistakes").innerText = mistakes;
+// function updateTimer() {
+//   if (time == 0) {
+//     //End test if timer reaches 0
+//     displayResult();
+//   } else {
+//     document.getElementById("timer").innerText = --time + "s";
+//   }
+// }
+
+
+// const timeReduce = () => {
+//   time = 60;
+//   timer = setInterval(updateTimer, 1000);
+// };
+
+// const displayResult = () => {
+
+//   document.querySelector(".result").style.display = "block";
+//   clearInterval(timer);
+//   document.getElementById("stop-test").style.display = "none";
+//   userInput.disabled = true;
+//   let timeTaken = 1;
+//   if (time != 0) {
+//     timeTaken = (60 - time) / 100;
+//   }
+//   document.getElementById("wpm").innerText =
+//     (userInput.value.length / 5 / timeTaken).toFixed(2) + " wpm";
+//   document.getElementById("accuracy").innerText =
+//     Math.round(
+//       ((userInput.value.length - mistakes) / userInput.value.length) * 100
+//     ) + " %";
+// };
+
+
+// const startTest = () => {
+//   mistakes = 0;
+//   timer = "";
+//   userInput.disabled = false;
+//   timeReduce();
+//   document.getElementById("start-test").style.display = "none";
+//   document.getElementById("stop-test").style.display = "block";
+// };
+
+// window.onload = () => {
+//   userInput.value = "";
+//   document.getElementById("start-test").style.display = "block";
+//   document.getElementById("stop-test").style.display = "none";
+//   userInput.disabled = true;
+//   renderNewQuote();
+// };
+
+$(document).ready(function () {
+  const quoteApiUrl = "https://api.quotable.io/random?minLength=80&maxLength=100";
+  const quoteSection = $("#quote");
+  const userInput = $("#quote-input");
+  let quote = "",
+    time = 60,
+    timer = "",
+    mistakes = 0;
+
+  quoteSection.oncopy = function (event) {
+    event.preventDefault();
+    return false;
+  };
+
+  const renderNewQuote = async () => {
+    try {
+      const { content } = await (await fetch(quoteApiUrl)).json();
+      renderQuote(content);
+    } catch (error) {
+      console.error('Error fetching quote:', error.message);
+    }
+  };
+
+  const renderQuote = (quote) => {
+    quoteSection.html([...quote].map(char => `<span class='quote-chars'>${char}</span>`).join(''));
+  };
+
+  userInput.on("input", function () {
+    let quoteChars = $(".quote-chars");
+    let userInputChars = userInput.val().split("");
+
+    quoteChars.each(function (index, char) {
+      char = $(char);
+      if (char.text() === userInputChars[index]) {
+        char.addClass("success");
+      } else if (!userInputChars[index]) {
+        char.removeClass("success fail");
+      } else {
+        if (!char.hasClass("fail")) {
+          mistakes += 1;
+          char.addClass("fail");
+          $("#mistakes").text(mistakes);
+        }
       }
+    });
+
+    if (quoteChars.toArray().every((element) => $(element).hasClass("success"))) {
+      displayResult();
     }
   });
 
-  if (quoteChars.every((element) => element.classList.contains("success"))) {
-    displayResult();
+  function updateTimer() {
+    if (time == 0) {
+      displayResult();
+    } else {
+      $("#timer").text(--time + "s");
+    }
   }
+
+  const timeReduce = () => {
+    time = 60;
+    timer = setInterval(updateTimer, 1000);
+  };
+
+  const displayResult = () => {
+    $(".result").css("display", "block");
+    clearInterval(timer);
+    $("#stop-test").css("display", "none");
+    userInput.prop("disabled", true);
+    let timeTaken = 1;
+    if (time != 0) {
+      timeTaken = (60 - time) / 100;
+    }
+    $("#wpm").text((userInput.val().length / 5 / timeTaken).toFixed(2) + " wpm");
+    $("#accuracy").text(Math.round(((userInput.val().length - mistakes) / userInput.val().length) * 100) + " %");
+  };
+
+  const startTest = () => {
+    mistakes = 0;
+    timer = "";
+    userInput.prop("disabled", false);
+    timeReduce();
+    $("#start-test").css("display", "none");
+    $("#stop-test").css("display", "block");
+  };
+
+  window.onload = () => {
+    userInput.val("");
+    $("#start-test").css("display", "block");
+    $("#stop-test").css("display", "none");
+    userInput.prop("disabled", true);
+    renderNewQuote();
+  };
+
+  $("#start-test").on("click", startTest);
+  $("#stop-test").on("click", displayResult);
 });
-
-
-//Update Timer on screen
-function updateTimer() {
-  if (time == 0) {
-    //End test if timer reaches 0
-    displayResult();
-  } else {
-    document.getElementById("timer").innerText = --time + "s";
-  }
-}
-
-//Sets timer
-const timeReduce = () => {
-  time = 60;
-  timer = setInterval(updateTimer, 1000);
-};
-
-//End Test
-const displayResult = () => {
-  //display result div
-  document.querySelector(".result").style.display = "block";
-  clearInterval(timer);
-  document.getElementById("stop-test").style.display = "none";
-  userInput.disabled = true;
-  let timeTaken = 1;
-  if (time != 0) {
-    timeTaken = (60 - time) / 100;
-  }
-  document.getElementById("wpm").innerText =
-    (userInput.value.length / 5 / timeTaken).toFixed(2) + " wpm";
-  document.getElementById("accuracy").innerText =
-    Math.round(
-      ((userInput.value.length - mistakes) / userInput.value.length) * 100
-    ) + " %";
-};
-
-//Start Test
-const startTest = () => {
-  mistakes = 0;
-  timer = "";
-  userInput.disabled = false;
-  timeReduce();
-  document.getElementById("start-test").style.display = "none";
-  document.getElementById("stop-test").style.display = "block";
-};
-
-window.onload = () => {
-  userInput.value = "";
-  document.getElementById("start-test").style.display = "block";
-  document.getElementById("stop-test").style.display = "none";
-  userInput.disabled = true;
-  renderNewQuote();
-};
